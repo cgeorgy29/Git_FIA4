973809550d5e93507d6ce6c47235256c939dd22a
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2022-11-22 09:33:11 +0100

    update 22/11/2022

---
+++ Antoine/Deep Learning/td1.py
@@ -0,0 +1,41 @@
+from matplotlib import pyplot as plt
+
+
+a = 2
+b = 2
+x = [23.8, 16.3, 27.2, 7.1, 25.1, 27.5, 19.4, 19.8, 32.2, 20.7]
+y = [115.4, 76.3, 113.8, 81.6, 115.4, 125, 83.6, 75.2, 136.8, 102.8]
+graph = []
+n = 300
+alpha = 0.001
+
+def dmsesurdacalc(x, y, yb):
+    sum = 0
+    n = len(x)
+    for i in range(n):
+        sum += x[i]*(y[i] - yb[i])
+    return -(2/n) * sum
+
+def dmsesurdbcalc(x, y, yb):
+    sum = 0
+    n = len(x)
+    for i in range(n):
+        sum += (y[i] - yb[i])
+    return -(2/n) * sum
+
+def calculy(x):
+    return a*x+b
+
+atab = []
+btab = []
+for i in range(n):
+    yb = list(map(calculy, x))
+    a = a-alpha*(dmsesurdacalc(x,y,yb))
+    b = b-alpha*(dmsesurdbcalc(x,y,yb))
+    print("a = " + str(a) + " b = " + str(b))
+    atab.append(a)
+    btab.append(b)
+
+plt.plot([0,50],[b, a*50+b])
+plt.scatter(x,y)
+plt.show()
